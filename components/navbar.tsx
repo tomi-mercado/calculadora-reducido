1873cbf488a79fbe4b99e1feb9b9ad2528631350
Tomas <tmercadoslp@gmail.com>
2024-11-03 01:24:54 -0300

    fix: title less bigger when is mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,9 +6,12 @@ export const Navbar = () => {
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 justify-between">
         <div className="flex gap-4 items-center">
           <Calculator />
-          <h1 className="text-2xl font-semibold">
+          <h1 className="text-2xl font-semibold hidden sm:block">
             Calculadora Reducido Primera Nacional
           </h1>
+          <p className="text-xl font-semibold sm:hidden">
+            Calculadora Reducido PN
+          </p>
         </div>
       </div>
     </header>
